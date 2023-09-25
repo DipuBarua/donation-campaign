0526efd41ecd68b0ad7d6b27ba00be131693a8b8
DipuBarua <you@example.com>
2023-09-25 17:26:33 +0600

    updated banner,fake donation data

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,11 @@
 const Banner = () => {
     return (
         <div>
-            
+            <h2 className=" text-5xl font-bold text-center mt-16">I Grow By Helping People In Need</h2>
+            <div className=" text-center mt-10">
+                <input type="text" placeholder="Search here..." className="input input-bordered w-full max-w-xs" />
+                <button type="submit" className="btn bg-[#FF444A]">Search</button>
+            </div>
         </div>
     );
 };
